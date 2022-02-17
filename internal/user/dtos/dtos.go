e90f64f01e61c57651f0a6f2fd356774658318a6
mauromamani <mauromamani.b@gmail.com>
2022-02-16 22:43:43 -0300

    chore: add createUserDto

---
+++ internal/user/dtos/dtos.go
@@ -0,0 +1,7 @@
+package dtos
+
+type CreateUserDto struct {
+	Name     string `json:"name" validate:"required"`
+	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"required"`
+}
