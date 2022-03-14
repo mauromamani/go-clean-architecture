a60bf5da24e39aa0919392f69d4dbbbfb2be23de
mauromamani <mauromamani.b@gmail.com>
2022-03-13 22:49:55 -0300

    feat: add maxBytesReader to 1MB

--- pkg/utils/http.go
+++ pkg/utils/http.go
@@ -1,12 +1,18 @@
 package utils
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 	"github.com/mauromamani/go-clean-architecture/pkg/validator"
 )
 
 // ReadRequest: Read request and validate
 func ReadRequest(c *gin.Context, req interface{}) error {
+	maxBytes := 1_048_576
+
+	var w http.ResponseWriter = c.Writer
+	c.Request.Body = http.MaxBytesReader(w, c.Request.Body, int64(maxBytes))
 	if err := c.BindJSON(req); err != nil {
 		return err
 	}
