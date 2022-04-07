308f9321f588d8b5a76164bda58b2bbbcca69c21
mauromamani <mauromamani.b@gmail.com>
2022-04-06 23:38:45 -0300

    feat: remove password for user entity JSON response

--- internal/user/entity/user_entity.go
+++ internal/user/entity/user_entity.go
@@ -6,6 +6,6 @@ type User struct {
 	ID        int64     `json:"id"`
 	Name      string    `json:"name"`
 	Email     string    `json:"email"`
-	Password  string    `json:"password"`
+	Password  string    `json:"-"`
 	CreatedAt time.Time `json:"created_at"`
 }
