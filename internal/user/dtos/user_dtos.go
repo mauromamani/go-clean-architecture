3542f6d556baf2a567375c8e3ee55a1a902d1ace
mauromamani <mauromamani.b@gmail.com>
2022-04-07 15:11:30 -0300

    chore: add anotations for update DTOs

--- internal/user/dtos/user_dtos.go
+++ internal/user/dtos/user_dtos.go
@@ -6,6 +6,7 @@ type CreateUserDto struct {
 	Password string `json:"password" validate:"required,passwd"`
 }
 
+// TODO: alphanum no permite usar espacios en blanco, se puede enviar un json vacio (no modifica en nada pero hay que verlo)
 type UpdateUserDto struct {
 	Name     *string `json:"name" validate:"omitempty,alphanum"`
 	Email    *string `json:"email" validate:"omitempty,required,email"`
