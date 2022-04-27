22dc7155f3eb562b345102728ebd3275d0dc23aa
mauromamani <mauromamani.b@gmail.com>
2022-04-26 22:56:21 -0300

    chore: add posts interface

--- internal/post/handler.go
+++ internal/post/handler.go
@@ -1,1 +1,11 @@
 package post
+
+import "net/http"
+
+type Handlers interface {
+	GetPosts(w http.ResponseWriter, r *http.Request)
+	GetPostById(w http.ResponseWriter, r *http.Request)
+	CreatePost(w http.ResponseWriter, r *http.Request)
+	UpdatePost(w http.ResponseWriter, r *http.Request)
+	DeletePost(w http.ResponseWriter, r *http.Request)
+}
--- internal/post/usecase.go
+++ internal/post/usecase.go
@@ -1,1 +1,15 @@
 package post
+
+import (
+	"context"
+
+	"github.com/mauromamani/go-clean-architecture/internal/post/entity"
+)
+
+type UseCase interface {
+	GetPosts(ctx context.Context) ([]*entity.Post, error)
+	GetPostById(ctx context.Context, id int64) (*entity.Post, error)
+	CreatePost(ctx context.Context, user *entity.Post) (*entity.Post, error)
+	UpdatePost(ctx context.Context, id int64, user *entity.Post) (*entity.Post, error)
+	DeletePost(ctx context.Context, id int64) error
+}
