61e61b957b87a0530c408d5b53e941217cbc8faa
mauromamani <mauromamani.b@gmail.com>
2022-04-26 23:08:23 -0300

    chore: add post_usecase

--- internal/post/usecase/post_usecase.go
+++ internal/post/usecase/post_usecase.go
@@ -1,1 +1,43 @@
 package usecase
+
+import (
+	"context"
+
+	"github.com/mauromamani/go-clean-architecture/internal/post"
+	"github.com/mauromamani/go-clean-architecture/internal/post/entity"
+)
+
+type postUseCase struct {
+	postRepository post.Repository
+}
+
+func NewPostUseCase(postRepo post.Repository) post.UseCase {
+	return &postUseCase{
+		postRepository: postRepo,
+	}
+}
+
+// GetPosts:
+func (uc *postUseCase) GetPosts(ctx context.Context) ([]*entity.Post, error) {
+	return nil, nil
+}
+
+// GetPostById:
+func (uc *postUseCase) GetPostById(ctx context.Context, id int64) (*entity.Post, error) {
+	return nil, nil
+}
+
+// CreatePost:
+func (uc *postUseCase) CreatePost(ctx context.Context, user *entity.Post) (*entity.Post, error) {
+	return nil, nil
+}
+
+// UpdatePost:
+func (uc *postUseCase) UpdatePost(ctx context.Context, id int64, user *entity.Post) (*entity.Post, error) {
+	return nil, nil
+}
+
+// DeletePost:
+func (uc *postUseCase) DeletePost(ctx context.Context, id int64) error {
+	return nil
+}
