720af28275f2b0bdd35e2e0fde9c41ecbbf842bd
mauromamani <mauromamani.b@gmail.com>
2022-04-26 23:14:03 -0300

    chore: add post package to app_handlers

--- internal/application/app_handlers.go
+++ internal/application/app_handlers.go
@@ -2,6 +2,9 @@ package application
 
 import (
 	"github.com/julienschmidt/httprouter"
+	postHttp "github.com/mauromamani/go-clean-architecture/internal/post/delivery/http"
+	postRepo "github.com/mauromamani/go-clean-architecture/internal/post/repository"
+	postUC "github.com/mauromamani/go-clean-architecture/internal/post/usecase"
 	userHttp "github.com/mauromamani/go-clean-architecture/internal/user/delivery/http"
 	userRepo "github.com/mauromamani/go-clean-architecture/internal/user/repository"
 	userUC "github.com/mauromamani/go-clean-architecture/internal/user/usecase"
@@ -11,13 +14,17 @@ import (
 func (app *application) mapHandlers() {
 	// init repositories
 	userRepository := userRepo.NewUserRepository(app.db)
+	postRepository := postRepo.NewPostRepository(app.db)
 
 	// init useCases
 	userUseCase := userUC.NewUserUseCase(userRepository)
+	postUseCase := postUC.NewPostUseCase(postRepository)
 
 	// init http handlers
 	userHandler := userHttp.NewUserHandlers(userUseCase, app.logger)
+	postHandler := postHttp.NewPostHandlers(postUseCase, app.logger)
 
 	// map Routes
 	userHttp.MapRoutes(app.srv.Handler.(*httprouter.Router), userHandler)
+	postHttp.MapRoutes(app.srv.Handler.(*httprouter.Router), postHandler)
 }
